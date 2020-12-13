df8a75ffe378768f739ef8c8e0c9258271bb053d
Gabriel Trocan <gabrieltrocan93@gmail.com>
2020-12-13 11:20:32 +0200

    added Account and Transaction entities

---
+++ src/main/java/com/gtrocan/atm/entity/Account.java
@@ -0,0 +1,29 @@
+package com.gtrocan.atm.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = {"transactions"})
+@EqualsAndHashCode(exclude = {"transactions"})
+@Builder
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long accountId;
+
+    @Column(nullable = false)
+    @Positive
+    private Integer accountBalance;
+
+    @OneToMany(mappedBy = "account", cascade = CascadeType.REMOVE)
+    private List<Transaction> transactions;
+}
---
+++ src/main/java/com/gtrocan/atm/entity/Transaction.java
@@ -0,0 +1,33 @@
+package com.gtrocan.atm.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Positive;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Positive
+    private Integer amount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @Column(nullable = false)
+    private LocalDateTime transactionDate;
+}
