f3f1e2666762cc839211e5640a4e61162da0f44c
Gabriel Trocan <gabrieltrocan93@gmail.com>
2020-12-13 13:17:36 +0200

    added h2 database configuration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+-- ADD Account
+
+INSERT INTO account (id, account_balance) VALUES (1,10000);
+
+-- ADD ATM Data
+INSERT INTO atm (id, location) VALUES (1,'Location1');
+
+INSERT INTO ATM_DENOMINATION_MAPPING (atm_id,amount,denomination) values (1,20,'500'), (1,20,'200'), (1,10,'100'), (1,100,'10') ;
+-- ADD Transaction
+
+INSERT INTO transaction (id, transaction_type, amount, account_id, transaction_date) VALUES (1, 'DEPOSIT_WITHDRAWAL', 1000, 1, '2020-12-05 18:10:50');
